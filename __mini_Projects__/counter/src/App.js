925051b112579b7ae6f3c8c73a3d9b96d9714aa2
AndriiKot <a_kocyuba@ukr.net>
2024-04-12 21:22:30 +0200

    'counter v.1.0.0'

--- __mini_Projects__/counter/src/App.js
+++ __mini_Projects__/counter/src/App.js
@@ -1,16 +1,23 @@
-import './normalize.css';
-import './index.css';
-import './App.css';
+import "./normalize.css";
+import "./index.css";
+import "./App.css";
+import React, { useState } from 'react';
 
 function App() {
-  let counter = 11;
+  let [counter, setCounter] = useState(0);
+
+  const counterPlus = () => setCounter(counter + 1);
+  const counterMinus = () => setCounter(counter - 1);
+  
   return (
     <div className="App">
       <div>
         <h2>Counter:</h2>
         <h1>{counter}</h1>
-        <button className="minus">- Minus</button>
-        <button className="plus">Plus +</button>
+        <button onClick={counterMinus} className="minus">- Minus</button>
+        <button onClick={counterPlus} className="plus">
+          Plus +
+        </button>
       </div>
     </div>
   );
