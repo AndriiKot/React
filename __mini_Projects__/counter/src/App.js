fcec46c5539c53eb72591ab28da2d63a5f7345ef
AndriiKot <a_kocyuba@ukr.net>
2024-04-12 20:56:20 +0200

    test var counter

--- __mini_Projects__/counter/src/App.js
+++ __mini_Projects__/counter/src/App.js
@@ -1,13 +1,14 @@
+import './normalize.css';
 import './index.css';
 import './App.css';
-import './normalize.css';
 
 function App() {
+  let counter = 11;
   return (
     <div className="App">
       <div>
         <h2>Counter:</h2>
-        <h1>0</h1>
+        <h1>{counter}</h1>
         <button className="minus">- Minus</button>
         <button className="plus">Plus +</button>
       </div>
