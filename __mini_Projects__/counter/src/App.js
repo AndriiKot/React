cdb1430ce896ff6314aaaa09f807de8cb20d0cb5
AndriiKot <a_kocyuba@ukr.net>
2024-04-15 13:37:42 +0200

    development of new functionality for the mini-project 'counter'

--- __mini_Projects__/counter/src/App.js
+++ __mini_Projects__/counter/src/App.js
@@ -1,20 +1,38 @@
 import "./normalize.css";
 import "./index.css";
 import "./App.css";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function App() {
   let [counter, setCounter] = useState(0);
 
   const counterPlus = () => setCounter(counter + 1);
   const counterMinus = () => setCounter(counter - 1);
-  
+
+  const createSpanElement = (event, count = 0) => {
+    for (let i = 0; i < count; i++) {
+      const spark = document.createElement("span");
+      spark.classList.add("spark");
+      event.target.append(spark);
+
+      setTimeout(() => spark.remove(), 2000);
+    }
+    console.dir(event.target);
+  };
+
+  const handleMinusClick = (event) => {
+    counterMinus();
+    createSpanElement(event, 50);
+  };
+
   return (
     <div className="App">
       <div>
         <h2>Counter:</h2>
         <h1>{counter}</h1>
-        <button onClick={counterMinus} className="minus">- Minus</button>
+        <button onClick={handleMinusClick} className="minus">
+          - Minus
+        </button>
         <button onClick={counterPlus} className="plus">
           Plus +
         </button>
