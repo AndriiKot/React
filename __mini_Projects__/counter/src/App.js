34ca17a68faaf6c7347212453fdc2e1bbf0933a4
AndriiKot <a_kocyuba@ukr.net>
2024-04-20 11:14:25 +0200

    'counter' split componet App step 2

--- __mini_Projects__/counter/src/App.js
+++ __mini_Projects__/counter/src/App.js
@@ -1,55 +1,11 @@
-import "./normalize.css";
 import "./index.css";
 import "./App.css";
-import { useState } from "react";
+import Counter from "./components/Counter";
 
 function App() {
-  let [counter, setCounter] = useState(0);
-
-  const counterPlus = () => setCounter(counter + 1);
-  const counterMinus = () => setCounter(counter - 1);
-
-  const createSpanElement = (event, count = 0) => {
-    for (let i = 0; i < count; i++) {
-      const square = document.createElement("span");
-      square.classList.add("square");
-      event.target.append(square);
-
-      const randomX = (Math.random() - 0.5) * window.innerWidth;
-      const randomY = (Math.random() - 0.5) * window.innerHeight;
-
-      square.style.setProperty("--x", randomX + "px");
-      square.style.setProperty("--y", randomY + "px");
-
-      square.style.height = Math.random() * (60 - 10) + 10 + "%";
-      setTimeout(() => square.remove(), 2000);
-    }
-  };
-
-  const handleMinusClick = (event,c = counter) => {
-    if (c < 0) { c *= -1; }
-    counterMinus();
-    createSpanElement(event, c + 1);
-  };
-
-  const handlePlusClick = (event,c = counter) => {
-    if (c < 0) { c *= -1; }
-    counterPlus();
-    createSpanElement(event, c + 1);
-  };
-
   return (
     <div className="App__counter">
-      <div className="wrapper">
-        <h2 className="counter__title">Counter:</h2>
-        <h1 className="counter__number">{counter}</h1>
-        <button onClick={handleMinusClick} className="minus">
-          - Minus
-        </button>
-        <button onClick={handlePlusClick} className="plus">
-          Plus +
-        </button>
-      </div>
+      <Counter />
     </div>
   );
 }
--- __mini_Projects__/counter/src/components/Counter/index.jsx
+++ __mini_Projects__/counter/src/components/Counter/index.jsx
@@ -1,6 +1,55 @@
-﻿import React from "react";
+﻿import { useState } from "react";
 import s from "./index.module.css";
 
 export default function Counter() {
-    return <div>Counter</div>;
+  let [counter, setCounter] = useState(0);
+
+  const counterPlus = () => setCounter(counter + 1);
+  const counterMinus = () => setCounter(counter - 1);
+
+  const createSpanElement = (event, count = 0) => {
+    for (let i = 0; i < count; i++) {
+      const square = document.createElement("span");
+      square.classList.add("square");
+      event.target.append(square);
+
+      const randomX = (Math.random() - 0.5) * window.innerWidth;
+      const randomY = (Math.random() - 0.5) * window.innerHeight;
+
+      square.style.setProperty("--x", randomX + "px");
+      square.style.setProperty("--y", randomY + "px");
+
+      square.style.height = Math.random() * (60 - 10) + 10 + "%";
+      setTimeout(() => square.remove(), 2000);
+    }
+  };
+
+  const handleMinusClick = (event, c = counter) => {
+    if (c < 0) {
+      c *= -1;
+    }
+    counterMinus();
+    createSpanElement(event, c + 1);
+  };
+
+  const handlePlusClick = (event, c = counter) => {
+    if (c < 0) {
+      c *= -1;
+    }
+    counterPlus();
+    createSpanElement(event, c + 1);
+  };
+
+  return (
+      <div className="wrapper">
+        <h2 className="counter__title">Counter:</h2>
+        <h1 className="counter__number">{counter}</h1>
+        <button onClick={handleMinusClick} className="minus">
+          - Minus
+        </button>
+        <button onClick={handlePlusClick} className="plus">
+          Plus +
+        </button>
+      </div>
+  );
 }
