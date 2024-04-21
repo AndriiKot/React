b002ac1905a57a01e1760a4b52f787d24326e1d5
AndriiKot <a_kocyuba@ukr.net>
2024-04-21 23:23:55 +0200

    'Counter' Signals version 'first test'

--- __mini_Projects__/Counter/__counter__Signals_version__/src/App.jsx
+++ __mini_Projects__/Counter/__counter__Signals_version__/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
+import { signal } from '@preact/signals-react'
+import Counter from "./components/Counter";
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <div className="App__counter">
+      <Counter />
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+
+
