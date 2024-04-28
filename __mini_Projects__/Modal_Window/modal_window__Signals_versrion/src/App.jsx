e4359a4cb4add88ec3aba2187cc5d6473c62ef7f
AndriiKot <a_kocyuba@ukr.net>
2024-04-28 10:15:02 +0200

    'Modal Window' testing 1 Signal version

--- __mini_Projects__/Modal_Window/modal_window__Signals_versrion/src/App.jsx
+++ __mini_Projects__/Modal_Window/modal_window__Signals_versrion/src/App.jsx
@@ -1,33 +1,16 @@
-import { signal } from "@preact/signals-react";
-import "./index.css";
+import { signal } from "@preact/signals-react"
+import './App.css'
 
-
+let test1 = signal(11)
 
 function App() {
-  let open = signal(false);
+
   return (
-    <div className="App">
-      <button className="open-modal-btn">
-        ✨ Open Modal
-      </button>
-      {open && (
-        <div className="overlay">
-          <div className="modal">
-            <svg
-              // onClick={() => signal(false)}
-              height="200"
-              viewBox="0 0 200 200"
-              width="200"
-            >
-              <title />
-              <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-            </svg>
-            <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
-          </div>
-        </div>
-      )}
-    </div>
-  );
+    <>
+      <h1>Modal Window</h1>
+      <div>signal {test1.value}</div>
+    </>
+  )
 }
 
-export default App;
+export default App
