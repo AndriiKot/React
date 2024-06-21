005a5f65b60de897612a7d258a40670cc787f1e3
AndriiKot <a_kocyuba@ukr.net>
2024-06-21 10:11:53 +0200

    PROJECTS 'React' !! update 'update_all_projects.rb'   !!

---
+++ update_all_projects.rb
@@ -0,0 +1,12 @@
+﻿projects = {
+ "Only_React_01": 'https://github.com/AndriiKot/React_only__01.git',
+ "Only_React_02": 'https://github.com/AndriiKot/React_only__02',
+}
+
+update = "git subtree pull --prefix "
+
+projects.each do |folder, git_repo| 
+    system(`#{update}#{folder} #{git_repo} main`)
+end
+
+system(`git push`)
