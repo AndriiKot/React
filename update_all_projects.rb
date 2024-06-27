080270a27d6d9c30ae57ba952c69d8298b0f3ad4
AndriiKot <a_kocyuba@ukr.net>
2024-06-27 09:55:26 +0200

    React !! UPDATE 'update_all_project.rb' added new project 'React Query 04'  !!

--- update_all_projects.rb
+++ update_all_projects.rb
@@ -2,6 +2,7 @@
  "Only_React_01": 'https://github.com/AndriiKot/React_only__01.git',
  "Only_React_02": 'https://github.com/AndriiKot/React_only__02',
  "React_03": "https://github.com/AndriiKot/React_03.git",
+ "React_Query_04": "https://github.com/AndriiKot/React_Query__01.git",
 }
 
 update = "git subtree pull --prefix "
