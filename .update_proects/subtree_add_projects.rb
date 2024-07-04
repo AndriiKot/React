d1ad3a6973958c148a88cb8ba5d25cc3c9250b16
AndriiKot <a_kocyuba@ukr.net>
2024-07-04 16:27:00 +0200

    React !! CREATE NEW FOLDEE '.update_proects' !!

---
+++ .update_proects/subtree_add_projects.rb
@@ -0,0 +1,9 @@
+﻿require_relative 'config'
+
+add  = "git subtree add --prefix "
+
+projects.each do |folder, git_repo| 
+    system(`#{update}#{folder} #{git_repo} main`)
+end
+
+system(`git push`)
