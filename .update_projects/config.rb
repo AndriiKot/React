a78755e146811aff948a857ba54891e16a2d5dbc
AndriiKot <a_kocyuba@ukr.net>
2024-07-04 19:03:12 +0200

    React !! update config.rb !!

--- .update_projects/config.rb
+++ .update_projects/config.rb
@@ -1,8 +1,8 @@
 ﻿PROJECTS = {
- "React_01__useState": 'https://github.com/AndriiKot/React_only__01.git',
+ "React_01__useState": 'https://github.com/AndriiKot/React_01__useState',
  "React_02__Cards_App": 'https://github.com/AndriiKot/React_02__Cards_App',
- "React_03__Fetch_API__Posts_App": "https://github.com/AndriiKot/React_03.git",
- "React_Query_01__Fetch_API__Posts_App": "https://github.com/AndriiKot/React_Query__01.git",
+ "React_03__Fetch_API__Posts_App": "https://github.com/AndriiKot/React_03__Fetch_API__Posts_App",
+ "React_Query_01__Fetch_API__Posts_App": "https://github.com/AndriiKot/React_Query_01__Fetch_API__Posts_App",
 }
 
 
