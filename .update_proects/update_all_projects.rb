293fc72bb69a5a7e2beab96160ef6338616398e8
AndriiKot <a_kocyuba@ukr.net>
2024-07-04 16:42:09 +0200

    React !! TESTING ADD subtree testing 2 !!

--- .update_proects/update_all_projects.rb
+++ .update_proects/update_all_projects.rb
@@ -1,13 +1,8 @@
-﻿projects = {
- "React_01__useState": 'https://github.com/AndriiKot/React_only__01.git',
- "React_02__Cards_App": 'https://github.com/AndriiKot/React_only__02',
- "React_03__Fetch_API__Posts_App": "https://github.com/AndriiKot/React_03.git",
- "React_Query_01__Fetch_API__Posts_App": "https://github.com/AndriiKot/React_Query__01.git",
-}
+﻿require_relative 'config'
 
 update = "git subtree pull --prefix "
 
-projects.each do |folder, git_repo| 
+PROJECTS.each do |folder, git_repo| 
     system(`cd .. && #{update}#{folder} #{git_repo} main`)
 end
 
