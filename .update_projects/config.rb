fd3df95ee8f7552df093ccc2a79e4f86b0657f12
AndriiKot <a_kocyuba@ukr.net>
2024-07-04 22:31:14 +0200

    React !! update config testing react 2 !!

--- .update_projects/config.rb
+++ .update_projects/config.rb
@@ -1,8 +1,8 @@
 ﻿PROJECTS = {
- "React_01__useState": 'https://github.com/AndriiKot/React_01__useState',
- "React_02__Cards_App": 'https://github.com/AndriiKot/React_02__Cards_App',
- "React_03__Fetch_API__Posts_App": "https://github.com/AndriiKot/React_03__Fetch_API__Posts_App",
- "React_Query_01__Fetch_API__Posts_App": "https://github.com/AndriiKot/React_Query_01__Fetch_API__Posts_App",
+ "React_01__useState" => 'https://github.com/AndriiKot/React_01__useState',
+ "React_02__Cards_App" => 'https://github.com/AndriiKot/React_02__Cards_App',
+ "React_03__Fetch_API__Posts_App" => "https://github.com/AndriiKot/React_03__Fetch_API__Posts_App",
+ "React_Query_01__Fetch_API__Posts_App" => "https://github.com/AndriiKot/React_Query_01__Fetch_API__Posts_App",
 }
 
 
