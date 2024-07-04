8feaa873f62b111c8f20107ade5e3b1696a59ac4
AndriiKot <a_kocyuba@ukr.net>
2024-07-04 16:14:23 +0200

    React !! update update_all_projtcts.rb !!

--- update_all_projects.rb
+++ update_all_projects.rb
@@ -1,8 +1,8 @@
 ﻿projects = {
- "Only_React_01": 'https://github.com/AndriiKot/React_only__01.git',
- "Only_React_02": 'https://github.com/AndriiKot/React_only__02',
- "React_03": "https://github.com/AndriiKot/React_03.git",
- "React_Query_04": "https://github.com/AndriiKot/React_Query__01.git",
+ "React_01__useState": 'https://github.com/AndriiKot/React_only__01.git',
+ "React_02__Cards_App": 'https://github.com/AndriiKot/React_only__02',
+ "React_03__Fetch_API__Posts_App": "https://github.com/AndriiKot/React_03.git",
+ "React_Query_01__Fetch_API__Posts_App": "https://github.com/AndriiKot/React_Query__01.git",
 }
 
 update = "git subtree pull --prefix "
