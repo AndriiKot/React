dbe441ef6683c8e04cb996d72fef46982a770731
AndriiKot <a_kocyuba@ukr.net>
2024-07-04 18:45:08 +0200

    React !! SPLIT NEW FUNCTIONAL testing 6 !!

--- .update_projects/subtree_add_projects.rb
+++ .update_projects/subtree_add_projects.rb
@@ -4,7 +4,13 @@ add  = "git subtree add --prefix "
 
 PROJECTS.each do |folder, git_repo| 
     if Dir.exist?("../#{folder}")
-        system(`cd .. && echo  #{folder} already exists in the parent directory `)
+#     Error !!!    system(`cd .. && echo "#{folder} already exists in the parent directory" `) !!!
+# 	  Error !!!    system(`cd .. && puts "#{folder} already exists in the parent directory" `) !!!
+#         !!! is not recognized as an internal or external command,
+#        operable program or batch file.  !!!!
+# Если папка с таким именем существует в родительском каталоге,
+#  то выводить в консоль сообщение "#{folder} already exists in the parent directory"
+# где 'folder' имя папки в родительском каталоге
     else
         system(`cd .. && #{add}#{folder} #{git_repo} main`)
     end
