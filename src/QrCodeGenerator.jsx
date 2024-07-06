d479577be2857cdd846e30fbe0910f8676675cac
AndriiKot <a_kocyuba@ukr.net>
2024-07-06 22:31:50 +0200

    React 'React 04' 'QR Code' ! create  'QrCodeComponent.jsx' Testing QR Code SVG !

--- src/QrCodeGenerator.jsx
+++ src/QrCodeGenerator.jsx
@@ -1,7 +1,10 @@
-﻿export const QrCodeGenerator = () => {
+﻿import { QRCodeSVG } from "qrcode.react";
+
+export const QrCodeGenerator = () => {
   return (
     <div>
       <h3>QR Code Generator</h3>
+      <QRCodeSVG value="Testing QR Code 123" />
     </div>
   );
 };
