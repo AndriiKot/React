ba59d88051a0eb27240103bf0ef9fd6dded7f47e
AndriiKot <a_kocyuba@ukr.net>
2024-07-06 22:51:08 +0200

    React 'React 04' 'QR Code' ! create  'QrCodeComponent.jsx' create 'onChange'  tesing 3 !

--- src/QrCodeGenerator.jsx
+++ src/QrCodeGenerator.jsx
@@ -1,19 +1,25 @@
 ﻿import { QRCodeSVG } from "qrcode.react";
 
 export const QrCodeGenerator = () => {
-    const ChangeValueTesting = "Testing Chage Value ";
-    const handleGenerate = () => {
-      console.log("Generate button clicked");
-    };
-    const onChangeGenerate = () => {
-      console.log("On Change button clicked");
-    };
+  const ChangeValueTesting = "Testing Chage Value ";
+  const handleGenerate = (event) => {
+    console.log("Generate button clicked", event);
+  };
+  const onChangeGenerate = (event) => {
+    console.log("On Change button clicked", event);
+  };
   return (
     <div>
       <h3>QR Code Generator</h3>
       <QRCodeSVG value="Testing QR Code 123" />
-      <input type="text" value={ChangeValueTesting} onChange={onChangeGenerate}/>
-      <button type="button" onClick={handleGenerate}>Generate</button>
+      <input
+        type="text"
+        value={ChangeValueTesting}
+        onChange={onChangeGenerate}
+      />
+      <button type="button" onClick={handleGenerate}>
+        Generate
+      </button>
     </div>
   );
 };
