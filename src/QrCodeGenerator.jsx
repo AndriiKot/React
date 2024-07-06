edc0f09677820a014ae664abb4b81b0dbdf2221b
AndriiKot <a_kocyuba@ukr.net>
2024-07-06 22:22:21 +0200

    React 'React 04' 'QR Code ! create  'QrCodeComponent.jsx'  testing 1 !

--- src/QrCodeGenerator.jsx
+++ src/QrCodeGenerator.jsx
@@ -1,1 +1,7 @@
-﻿
+﻿export const QrCodeGenerator = () => {
+  return (
+    <div>
+      <h1>QR Code Generator</h1>
+    </div>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Layout } from "./Layout";
+import { QrCodeGenerator } from "./QrCodeGenerator";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
     <h1>QR Code</h1>
     <Layout />
+    <QrCodeGenerator />
   </>
 );
