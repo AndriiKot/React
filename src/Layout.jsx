88544a32979d8481d52f9a34886a67cdc9e73be1
AndriiKot <a_kocyuba@ukr.net>
2024-07-06 22:04:54 +0200

    React 'React 04' 'QR Code ! Create new componetn 'Layout.jsx' !

---
+++ src/Layout.jsx
@@ -0,0 +1,7 @@
+﻿export const Layout = () => {
+  return (
+    <div>
+      <h1>Layout</h1>
+    </div>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Layout } from './Layout'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div>QR Code</div>
