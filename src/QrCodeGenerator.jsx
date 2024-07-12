15ac200ba5dadc55f983ea12ce465ff247eb13e5
AndriiKot <a_kocyuba@ukr.net>
2024-07-12 18:48:25 +0200

    React 'React 04' 'QR Code' ! input value genrate qrcode handleClick testing 5 fixed bag  !

--- src/QrCodeGenerator.jsx
+++ src/QrCodeGenerator.jsx
@@ -3,22 +3,23 @@ import { QRCodeSVG } from "qrcode.react";
 
 export const QrCodeGenerator = () => {
   const [inputValue, setInputValue] = useState("");
-  const [handleClick, setHandleClick] = useState(false);
+  const [handleClick, setHandleClick] = useState('');
 
   const handleGenerate = () => {
-    setHandleClick(!handleClick);
+    setHandleClick(inputValue);
+    setInputValue("");
   };
 
   const onChangeGenerate = (event) => {
     setInputValue(event.target.value);
+    setHandleClick('');
   };
 
-  console.log(inputValue);
-  
+
   return (
     <div>
       <h3>QR Code Generator</h3>
-      {handleClick && <QRCodeSVG value={inputValue} />}
+      {handleClick !== '' && <QRCodeSVG value={handleClick} />}
       <input type="text" value={inputValue} onChange={onChangeGenerate} />
       <button type="button" onClick={handleGenerate}>
         Generate
