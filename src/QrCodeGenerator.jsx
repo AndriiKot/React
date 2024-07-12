2e36cbdc77de62b8d2decfeb6173af62d1fe31e6
AndriiKot <a_kocyuba@ukr.net>
2024-07-12 22:26:23 +0200

    React 'React 04' 'QR Code' ! testing block hidden testing 1  !

--- src/QrCodeGenerator.jsx
+++ src/QrCodeGenerator.jsx
@@ -5,19 +5,26 @@ import "./QrCodeGenerator.css";
 export const QrCodeGenerator = () => {
   const [inputValue, setInputValue] = useState("");
   const [qrCodeValue, setQrCodeValue] = useState("");
+  const [isHidden, setIsHidden] = useState(false);
 
   const handleGenerate = () => {
     setQrCodeValue(inputValue);
     setInputValue("");
+    setIsHidden(true);
   };
 
   const onChangeGenerate = (event) => {
     setInputValue(event.target.value);
     setQrCodeValue("");
+    setIsHidden(false);
   };
 
   return (
     <div className="wrapper__generator">
+      <div
+        className="test"
+        style={{ display: isHidden ? "none" : "block" }}
+      ></div>
       {qrCodeValue !== "" && <QRCodeSVG value={qrCodeValue} />}
       <input type="text" value={inputValue} onChange={onChangeGenerate} />
       <button type="button" onClick={handleGenerate}>
